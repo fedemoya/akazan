9980f332c8a0e8b2a7b762b0423306007eab4d03
Federico Moya <federico.moya@willdom.com>
2018-07-18 17:41:23 -0300

    First commit. OpenXava stuff. Persistence config. First entity: Client.

---
+++ src/com/akazan/model/Client.java
@@ -0,0 +1,93 @@
+package com.akazan.model;
+
+import javax.persistence.*;
+
+import org.openxava.annotations.*;
+
+@Entity
+public class Client {
+
+	private static final String SEQ_NAME = "client_seq";
+
+	@Id
+	@SequenceGenerator(name = SEQ_NAME, catalog = "akazan", sequenceName = SEQ_NAME, allocationSize = 1, initialValue = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQ_NAME)
+	private Long id;
+
+	@Required
+	private String name;
+	@Required
+	private String lastname;
+
+	private String mobileNumber;
+
+	private String addressCity;
+	private String addressStreet;
+	private String addressNumber;
+	private String addressApartment;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getAddressCity() {
+		return addressCity;
+	}
+
+	public void setAddressCity(String addressCity) {
+		this.addressCity = addressCity;
+	}
+
+	public String getAddressStreet() {
+		return addressStreet;
+	}
+
+	public void setAddressStreet(String addressStreet) {
+		this.addressStreet = addressStreet;
+	}
+
+	public String getAddressNumber() {
+		return addressNumber;
+	}
+
+	public void setAddressNumber(String addressNumber) {
+		this.addressNumber = addressNumber;
+	}
+
+	public String getAddressApartment() {
+		return addressApartment;
+	}
+
+	public void setAddressApartment(String addressApartment) {
+		this.addressApartment = addressApartment;
+	}
+
+}
